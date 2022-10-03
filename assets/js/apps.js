63c385e5ca5c2dd4514c18e8482d3587a783c488
Hernan Tapia <soliditize@MacBook-Pro.local>
2022-10-02 22:55:29 -0700

    Added Variables, Conditionals, window prompts

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,24 +1,17 @@
 // Assignment code here
 
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
 
 
 // WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
+
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
+
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
+
+
 
 
 //  Create Vars for lowercase, uppercase, numeric values and special characters
@@ -30,27 +23,73 @@ var alphabetUpper = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'
     'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
     'Y', 'Z' ]
 
-var alphabetLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q", 
-"r","s","t","u","v","w","x","y","z"];
+var alphabetLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q", "r","s","t","u","v","w","x","y","z"];
 
 
-// console.log(alphabetUpper);
-// console.log(alphabetLower);
-
-
+var randomPassword = [];
 
 function messagePrompt(){
 
-window.prompt('Would you like to generate a random password?');
-window.prompt('Your password length should be between 8 and 128 charcters.');
-window.prompt('Would you like to include lowercase?');
-window.prompt('Would you like to include uppercase?');
-window.prompt('Would you like a numeric value?');
-window.prompt('Would you like special characters?');
+var userLengthInput = window.prompt('How long do you want your password to be?');
+
+var passwordLength = parseInt(userLengthInput);
+
+if(isNaN(userLengthInput)){
+    window.alert('This is not a number ')
+    return
+}
+if (passwordLength < 8 || passwordLength > 128){
+    window.alert('Password length must be between 8 and 128.')
+    return
+}
+
+// console.log(passwordLength);  <--- Testing purposes 
+
+
+// Variables for prompt messages
+
+userInputLowerCase = window.confirm('Would you like to include lowercase?');
+userInputUpperCase = window.confirm('Would you like to include uppercase?');
+userInputNumericValue = window.confirm('Would you like a numeric value?');
+userInputSpecialCharacters = window.confirm('Would you like special characters?');
+
+if (userInputLowerCase === true){
+    randomPassword.push(alphabetLower)
+}
+
+if (userInputUpperCase === true){
+    randomPassword.push(alphabetUpper)
+}
+
+if (userInputNumericValue === true){
+    randomPassword.push(numericValues)
+}
+
+if (userInputSpecialCharacters === true){
+    randomPassword.push(specialCharacters)
+}
+
+console.log(randomPassword)
 
 }
 
 messagePrompt();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
